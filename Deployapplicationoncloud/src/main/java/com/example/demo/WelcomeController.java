dcaf63df032a5d768a5ae796ef353fe2b3f2dbe1
Hasrh <arasuharshini201@gmail.com>
2022-10-12 19:42:27 +0530

    pahse5 Deploy application on Cloud

---
+++ Deployapplicationoncloud/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class WelcomeController {
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome Spring Boot Application on AWS Cloud";
+	}
+
+}
